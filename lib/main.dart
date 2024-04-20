549ae403cdf7c91cea82d7c54525266d546986d9
abdoahmed <abdoahmed22231ad@gmail>
2024-04-20 06:26:33 +0200

    add in master

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ResponsiveDashBoard(),
+      home: Row(
+        children: [
+          const ResponsiveDashBoard(),
+        ],
+      ),
+
     );
   }
 }
